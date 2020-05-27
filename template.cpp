41987cb63d178c164d2da4a7dc18ea9fecab124f
Joseph Hynimoto Aguilar Lopez <50306337+JosepHyv@users.noreply.github.com>
2020-05-26 21:00:00 -0500

    Add files via upload

---
+++ template.cpp
@@ -0,0 +1,48 @@
+/*
+code template that i made 
+for use in codeforces or others online contest
+
+ID: JosepHy	
+LANG: C++14
+
+*/
+#include<set>
+#include<cmath>
+#include<queue>
+#include<vector>
+#include<string>
+#include<climits>
+#include<cstdlib>
+#include<iostream>
+#include<algorithm>
+#define f first 
+#define s second
+#define All(x,n) x,x+n
+#define all(x) x.begin() , x.end()
+#define p(dat1, dat2) pair< dat1, dat2> 
+using namespace std;
+
+
+typedef long long int ll;
+
+const ll MAXN = ll( 1e6)+3;
+const ll INF = (1ll << 32 );
+
+ll t,n,k;
+
+void solve()
+{
+	return 0;
+}
+
+main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL);
+	
+	cin>>t;
+	while( t--)
+	{
+
+	}
+
+}
