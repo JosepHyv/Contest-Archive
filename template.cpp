a71b8fa1b41568ceea149ce085ac210f7d8ad36b
JosepHyv <josephynimoto@gmail.com>
2020-07-06 14:41:55 -0500

    updated

--- template.cpp
+++ template.cpp
@@ -5,10 +5,9 @@ ID: JosepHy
 LANG: C++17
 INSPIRATION: Vanessa Rodriguez Medina 
 */
-
-// add Pragma for optimize the GCC compilation
+/* add pragmas is not recomended but you can (:
 #pragma GCC optimize ("-O3")
-#pragma GCC optimize ("unroll-loops")
+#pragma GCC optimize ("unroll-loops")*/
 #include<set>
 #include<map>
 #include<list>
@@ -26,38 +25,35 @@ INSPIRATION: Vanessa Rodriguez Medina
 #define all(x) x.begin() , x.end()
 #define mcm(a,b) a*b/__gcd(a,b);
 #define pb(x) push_back(x)
+
 using namespace std;
 
 typedef double db;
 typedef long long int ll;
 typedef vector < int > vi;
-typedef vector < ll > vll;
-typedef pair< ll, ll > pll;
-typedef pair< int , int > pii;
+typedef vector < ll > vl;
+typedef pair < ll, ll > pll;
+typedef pair < int , int > pii;
 const ll mod = 1e9+7;
 const ll MAXN = 1e6+3;
 const ll inf = (1ll << 32 );
 const ll ninf = (-1ll << 32);
+
 
 /// in most problems always use int's for principal variables
 int  t,n,k;
 
 void solve()
 {
-	
 	return;
 }
 
 int main()
 {
 	
-	ios_base::sync_with_stdio(false); cin.tie(NULL);
-	
-	cin>>t;
-	while( t--)
-	{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
 
-		solve();
-	}
+	cin>>t;
 
 }
