108cf8eb7e5287777e1249850110d1752f84f349
JosepHyv <josephynimoto@gmail.com>
2020-07-22 23:02:51 -0500

    Short template, slow compilation

--- short-template.cpp
+++ short-template.cpp
@@ -3,7 +3,7 @@
 #include<bits/stdc++.h>
 #define fi first 
 #define se second
-#define pb(x) push_back(x)
+#define pb push_back
 #define all(x) x.begin(), x.end()
 #define mcm( a, b) a*b/__gcd(a,b)
 using namespace std;
@@ -16,6 +16,7 @@ typedef vector < bool > mrk;
 typedef pair < ll, ll > pll;
 typedef pair < int , int > pii;
 typedef vector< vector< ll >> adj;
+typedef unsigned long long int ull;
 constexpr ll mod = 1e9+7;
 constexpr ll MAXN = 1e6+3;
 constexpr ll inf = 1ll << 32;
@@ -24,6 +25,5 @@ int main()
 {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
-
 	
 }
