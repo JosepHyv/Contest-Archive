3560b8caf31e6dfe08578e724dbcc6def552c3d9
JosepHyv <josephynimoto@gmail.com>
2020-09-09 02:39:39 -0500

    template that i use for cses problemset

---
+++ cses-template.cpp
@@ -0,0 +1,16 @@
+//TEMPLATE FOR CSES PROBLEMSET
+//PROBLEM LINK:
+//PROBLEM PATH: 
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+constexpr int MAXN = 1e6;
+constexpr int inf = 2e9;
+
+int main()
+{	
+	ios::sync_with_stdio(false); 
+	cin.tie(0);
+	
+}
