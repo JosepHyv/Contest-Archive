3d40cf0f0f7e02ece2ff10a686c2356562d8a748
JosepHyv <josephynimoto@gmail.com>
2021-08-19 13:47:40 -0500

    Upate

--- codeforces.cpp
+++ codeforces.cpp
@@ -1,102 +1,54 @@
-//PROBLEM LINK:
-//PROBLEM PATH:
 #include<bits/stdc++.h>
-#define fi first 
+#define fi first
 #define se second
 #define pb push_back
-#define pob pop_back
-#define pof pop_front
-#define pf push_front
-#define sz(x) (int) x.size()
-#define space(x) getline(cin,x)
+#define sz(x) (int)x.size()
 #define all(x) x.begin(), x.end()
-#define mcm( a, b) a*b/__gcd(a,b)
-#define adj(x) vector<  vector < x > >  
-#define clr(x) memset( x, 0, sizeof(x))
-#define press(x) setprecision(x)<<fixed
-#define deb(x) cerr<< #x<< "is "<< x<<"\n"
+#define rall(x) x.rbegin(), x.rend()
+#define mset(x,y) memset(x,y,sizeof(x))
 using namespace std;
 
-typedef long double ld;
-typedef set < int > si;
-typedef vector< int > vi;
-typedef long long int ll;
-typedef set < string > ss;
-typedef pair < ll, ll > pll;
-typedef pair < int , int > pii;
-typedef unsigned long long int ull;
-
-void DBG()
-{
-	cerr<<"]\n";
-	return;
-}
-
+void DBG(){	cerr<<")\n";}
 template<class H, class... T > 
-void DBG( H h, T... t)
-{
-	cerr << h;
-	if( sizeof...(t))
-		cerr<<", ";
-	DBG(t...);
-}
-#define dbg(...) cerr <<" values[ "<< #__VA_ARGS__ << " ] ---> [ ", DBG(__VA_ARGS__)
+void DBG( H h, T... t){cerr << h;if( sizeof...(t))cerr<<", ";DBG(t...);}
+#define dbg(...) cerr <<" values[ "<< #__VA_ARGS__ << " ] = ( ", DBG(__VA_ARGS__)
+template <class T > 
+void dbgv(vector < T > &x){cerr<<"[ ";for( auto a: x) cerr<<a<<","[a==x.back()]<<" "; cerr<<"]\n"; }
 
-template < class A > void read( vector < A >& v);
-template < class A, size_t S > void read( array < A , S>& a);
 
-template < typename... T> 
-void read(T&... args)
-{
-	((cin >> args), ...);
-}
-
-template < class A >
-void read( vector< A > & x)
-{
-	for( auto &a: x)
-		read(a);
-}
-template < class A, size_t S> 
-void read( array < A, S > &x)
-{
-	for(auto &a: x)
-		read(a);
-}
-
-template < typename... T > 
-void print(T... args)
-{
-	((cout << args <<" "), ...);
-}
-
-template < typename... T > 
-void write(T... args)
-{
-	((cout<<args<<"\n"), ...);
-}
-template < class A > 
-void print(vector< A >&x)
-{
-	for(auto v:x)
-		print(v);
-}
-
-constexpr ll mod = 1e9+7;
-constexpr ll MAXN = 1e6+3;
+typedef long double ld; 
+typedef long long int ll;
+typedef unsigned long long int ul;
+constexpr int MAXN = 1e6+2;
 constexpr int inf = 2e9;
-constexpr short dx4[] = {-1,0,1,0};
-constexpr short dy4[] = {0,1,0,-1};
-constexpr short dx8[] = {-1,-1,-1,0,1,1,1,0};
-constexpr short dy8[] = {-1,0,1,1,1,0,-1,-1};
 
+
+///aqui puede ir algo 
 
 
 int main()
 {
 	ios::sync_with_stdio(false);
-	cin.tie(0);
+	cin.tie(NULL);
+	/*freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);*/
 
 	
 	
+
+	cout<<"\n";
+	///uwu - vrm - Vanessa Rodriguez Medina <3
 }
+/* [°-°]  <- tss 
+   [./../] <- este mensaje puede cambiar
+	
+	by Benq
+	COSAS QUE DEBERIAS BUSCAR
+	* desbordamientos de int, rango de los arreglos
+	* casos especiales ( n = 1? )
+	* haz algo en lugar de nada, mantente organizado
+	* ESCRIBE COSAS E IDEAS ABAJO
+	* NO TE CASES CON UNA IDEA O ENFOQUE
+
+*/
+
