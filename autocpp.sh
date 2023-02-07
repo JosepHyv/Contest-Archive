b36adbbe4be378f8c0c5829e3dddfca665c1deee
josephyv <josephynimoto@gmail.com>
2023-02-07 13:24:34 -0600

    provitional basic automatization

---
+++ autocpp.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/sh
+
+TEMPLATE_URL=/home/josephy/Contest-Archive/CPTEMPLATES/cpUSACO.cpp
+current=`pwd`
+file=$1
+echo 'creating ${file}.cpp'
+file=$file.cpp
+echo 'copping from ${TEMPLATE_URL} to ${current}'
+cp $TEMPLATE_URL $file
+echo 'openning ${file}'
+subl $file
+
