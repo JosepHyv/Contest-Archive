c4c2f201963080f56566d1f4d05f3d60cd060b42
josephyv <josephynimoto@gmail.com>
2023-02-08 09:44:55 -0600

    new problems and automatization

--- autocpp.sh
+++ autocpp.sh
@@ -3,10 +3,10 @@
 TEMPLATE_URL=/home/josephy/Contest-Archive/CPTEMPLATES/cpUSACO.cpp
 current=`pwd`
 file=$1
-echo 'creating ${file}.cpp'
+echo "creating $file.cpp"
 file=$file.cpp
-echo 'copping from ${TEMPLATE_URL} to ${current}'
+echo "copping from $TEMPLATE_URL to $current"
 cp $TEMPLATE_URL $file
-echo 'openning ${file}'
+echo "openning $file"
 subl $file
 
